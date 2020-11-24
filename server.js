d33a36d43f029d06634c0fa6c8bab993955f6c97
Donato Renzulli <donren.99@gmail.com>
2020-11-24 22:58:07 +0100

    porta

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ let socket = require("socket.io");
 
 let app = express();
 
-let port = process.env.PORT || 3000;
+// let port = process.env.PORT || 3000;
+let port =3000;
 
 
 let  server = app.listen(port);
