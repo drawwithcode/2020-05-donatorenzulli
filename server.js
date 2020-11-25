94d71f3cdd070cfae6eb35f8048cf0bd5d563f0c
Donato Renzulli <donren.99@gmail.com>
2020-11-25 18:32:11 +0100

    File aggiunti

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let express = require("express");
 let socket = require('socket.io');
 
 let app = express();
-let port = 3000;
+let port = process.env.PORT || 3000;
 let server = app.listen(port);
 app.use(express.static("public"))
 
@@ -20,7 +20,7 @@ function newConnection(socket) {
   let clientColor = getRandomColor()
 
   socket.emit("color", clientColor)
-  
+
   //send the color to all the other clients
   socket.broadcast.emit("newPlayer", clientColor)
 
